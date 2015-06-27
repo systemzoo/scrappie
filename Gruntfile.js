4f3cdb81cac33aa48aa3339a8b02d1e4fb068a33
Charles Butler <chuck@dasroot.net>
2015-06-27 05:52:22 -0400

    Adds project linters and validation for CI

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: ['*.js', 'scripts/*.js']
+        },
+        coffeelint: {
+            options: {
+                'max_line_length': {
+                    'level': 'ignore'
+                },
+            },
+            all: ['scripts/*.coffee']
+        },
+        exec: {
+            hubot: 'bin/hubot -t',
+            npm_purge: 'rm -rf node_modules/*',
+        },
+        jsonlint: {
+            dependencies: {
+                src: [ '*.json']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jsonlint');
+    grunt.loadNpmTasks('grunt-coffeelint');
+    grunt.loadNpmTasks('grunt-exec');
+
+
+    grunt.registerTask('default', ['jsonlint', 'jshint', 'coffeelint', 'exec']);
+};
